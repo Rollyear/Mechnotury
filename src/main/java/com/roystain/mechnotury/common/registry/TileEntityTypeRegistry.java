849b4f96eace8238e1027fc8d2ee6cbd655fc70c
Rollyear <2661052482.qq.com>
2020-08-28 18:10:42 +0800

    Update the Registries

--- src/main/java/com/roystain/mechnotury/common/registry/TileEntityTypeRegistry.java
+++ src/main/java/com/roystain/mechnotury/common/registry/TileEntityTypeRegistry.java
@@ -1,5 +1,6 @@
 package com.roystain.mechnotury.common.registry;
 
+import com.roystain.mechnotury.common.Mechnotury;
 import com.roystain.mechnotury.common.block.machine.forgingtable.ForgingTableTileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
@@ -7,9 +8,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class TileEntityTypeRegistry {
-    public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = new DeferredRegister<>(ForgeRegistries.TILE_ENTITIES, "mechnotury");
-    public static RegistryObject<TileEntityType<ForgingTableTileEntity>> forgingTableTileEntity = TILE_ENTITIES
-            .register("forging_table_tileentity", () -> {
-        return TileEntityType.Builder.create(() -> new ForgingTableTileEntity(), BlockRegistry.forgingTable.get()).build(null);
-    });
+    public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = new DeferredRegister<>(ForgeRegistries.TILE_ENTITIES, Mechnotury.modId);
+    public static RegistryObject<TileEntityType<ForgingTableTileEntity>> forgingTableTileEntity = TILE_ENTITIES.register("forging_table", () -> TileEntityType.Builder.create(
+            ForgingTableTileEntity::new, BlockRegistry.forgingTable.get()).build(null));
 }
