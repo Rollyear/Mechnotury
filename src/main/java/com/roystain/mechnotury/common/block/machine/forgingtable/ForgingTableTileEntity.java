e3c7e4603046a8a6eb31127dfe9e29dd32d20224
Rollyear <2661052482.qq.com>
2020-08-31 17:57:31 +0800

    Delete the Container

--- src/main/java/com/roystain/mechnotury/common/block/machine/forgingtable/ForgingTableTileEntity.java
+++ src/main/java/com/roystain/mechnotury/common/block/machine/forgingtable/ForgingTableTileEntity.java
@@ -1,31 +1,19 @@
 package com.roystain.mechnotury.common.block.machine.forgingtable;
 
 import com.roystain.mechnotury.common.registry.TileEntityTypeRegistry;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
-import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.common.util.Constants;
 
 import javax.annotation.Nullable;
 import java.util.Objects;
 
-public class ForgingTableTileEntity extends TileEntity implements ITickableTileEntity, INamedContainerProvider {
-
+public class ForgingTableTileEntity extends TileEntity {
     //???
     public ItemStack itemStack = ItemStack.EMPTY;
-    public ItemStack getItemStack() {
-        return itemStack;
-    }
     //渲染方面的东西
     public ForgingTableTileEntity(){
         super(TileEntityTypeRegistry.forgingTableTileEntity.get());
@@ -61,11 +49,16 @@ public class ForgingTableTileEntity extends TileEntity implements ITickableTileE
     //客户端接收数据包
     @Override
     public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+        super.onDataPacket(net,pkt);
         handleUpdateTag(pkt.getNbtCompound());
     }
     @Override
     public void handleUpdateTag(CompoundNBT tag) {
         this.itemStack = ItemStack.read(tag).copy();
+    }
+    //获取物品信息
+    public ItemStack getItemStack() {
+        return itemStack;
     }
     //放入物品
     public ItemStack pushItemStack() {
@@ -83,22 +76,4 @@ public class ForgingTableTileEntity extends TileEntity implements ITickableTileE
         Objects.requireNonNull(world).notifyBlockUpdate(this.pos, this.getBlockState(),
                 this.getBlockState(), Constants.BlockFlags.BLOCK_UPDATE);
     }
-    //Container
-    public ITextComponent getDisplayName() {
-        return new StringTextComponent("Forging Container");
-    }
-    @Nullable
-    public Container createMenu(int sycID, PlayerInventory inventory, PlayerEntity player) {
-        return new ForgingContainer(sycID, inventory, this.pos, this.world, itemNumber);
-    }
-    public Inventory getInventory() {
-        return inventory;
-    }
-    public void tick() {
-        if (!world.isRemote) {
-            this.itemNumber.set(0, this.inventory.getStackInSlot(0).getCount());
-        }
-    }
-    private Inventory inventory = new Inventory(1);
-    private ForgingContainerItemNumber itemNumber = new ForgingContainerItemNumber();
 }
