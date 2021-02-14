1223502fffc9452a98e193913d166cd17c0decf6
Rollyear <2661052482.qq.com>
2021-02-14 10:53:16 +0800

    Change a bit things

--- src/main/java/com/roystain/mechnotury/common/Mechnotury.java
+++ src/main/java/com/roystain/mechnotury/common/Mechnotury.java
@@ -3,8 +3,9 @@ package com.roystain.mechnotury.common;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import com.roystain.mechnotury.common.registry.BlockRegistry;
 import com.roystain.mechnotury.common.registry.ItemRegistry;
+import com.roystain.mechnotury.common.registry.BlockRegistry;
+
 @Mod(Utils.MOD_ID)
 public class Mechnotury {
     public Mechnotury(){
